22e84d86123e9d2867c973d90b7c7e9422be9d6d
nthnyvllflrs <anthonvillaflor@gmail.com>
2021-02-17 22:30:32 +0800

    feat: seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\User::class)->create(['role' => 'TEACHER'])->each(function($user) {
+            factory(\App\Teacher::class)->create(['user_id' => $user->id])->each(function($teacher) {
+                factory(\App\Section::class)->create(['teacher_id' => $teacher->id])->each(function($section) {
+                    factory(\App\Student::class)->create(['section_id' => $section->id])->each(function($student) {
+                        factory(\App\Attendance::class)->create(['student_id' => $student->id, 'status' => 'PRESENT', 'date' => '2021-12-12']);
+                    });
+                });
+            });
+        });
     }
 }
